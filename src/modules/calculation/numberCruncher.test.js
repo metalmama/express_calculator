a4040858b855337833e9b0f3e361073d3aa33338
metalmama <sofie@callycode.com>
2022-09-05 09:48:11 +0200

    unit test only

--- src/modules/calculation/numberCruncher.test.js
+++ src/modules/calculation/numberCruncher.test.js
@@ -1,5 +1,4 @@
 import numberCruncher from "./numberCruncher.js"
-import percentage from ".util/percentage.js"
 
 describe('numberCruncher unit tests', () => {
 
@@ -8,11 +7,5 @@ describe('numberCruncher unit tests', () => {
 	 expect(numberCruncher(data)).toBe(9)
 
  })
-
-describe("numberCruncher integration tests", () => {
-
- it ('it should return 50 when given 1 and 2', () =>
- 	const data = { numOne: 1, operator: "percentage", numTwo:2 }
- 	expect(numberCruncher(data)).toBe(50)
 
 })
