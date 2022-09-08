5c0896893ac59f17163e4632ad54db3f9cdc4cf4
metalmama <sofie@callycode.com>
2022-09-08 14:52:32 +0200

    fixed paths till util

--- __tests__/unit/numberCruncher.test.js
+++ __tests__/unit/numberCruncher.test.js
@@ -1,4 +1,4 @@
-import numberCruncher from "../../modules/calculation/numberCruncher.js"
+import numberCruncher from "../../src/modules/calculation/numberCruncher.js"
 
 
 describe('numberCruncher unit tests', () => {
